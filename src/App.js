a348487e7ef796c537ca661e978d77f20ab663ae
Maha Mahmood <>
2021-08-17 18:53:15 -0400

    init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+import logo from './logo.svg';
+import './App.css';
+import Products from "./Components/products"
+
+
+function App() {
+  return (
+  <Products></Products>
+  );
+}
+
+export default App;
